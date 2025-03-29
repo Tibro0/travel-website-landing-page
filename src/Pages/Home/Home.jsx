8ceb4a4f6ba417d57915865f3640649d2a26c60b
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-03-29 15:30:35 +0600

    Recommended Section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css';
 import HeroSection from '../../Components/HeroSection/HeroSection';
 import SearchForm from '../../Components/SearchForm/SearchForm';
 import LastHolidaysSection from '../../Components/LastHolidaysSection/LastHolidaysSection';
+import RecommendedSection from '../../Components/RecommendedSection/RecommendedSection';
 
 function Home() {
     return (
@@ -10,6 +11,9 @@ function Home() {
             <HeroSection />
             <SearchForm/>
             <LastHolidaysSection/>
+            <div className='my-4 my-sm-5'>
+                <RecommendedSection itemRecommendedTitle='Recommended for you'/>
+            </div>
         </div>
     )
 }
